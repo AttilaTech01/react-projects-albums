4a2bf29caa6407e0743c15257437e4de20207f40
AttilaTech01 <d.hains@attila-technologies.com>
2024-05-29 08:23:07 -0400

    Initial commit, db and thunks are working

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// AUTOMATICALLY ADDED
+// fetchUsers.pending === 'users/fetch/pending'
+// fetchUsers.fulfilled === 'users/fetch/fulfilled'
+// fetchUsers.rejected === 'users/fetch/rejected'
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response = await axios.get('http://localhost:3005/users');
+
+    // DEV ONLY - fake lag time
+    await pause(3000);
+
+    return response.data;
+});
+
+// DEV ONLY
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
+export { fetchUsers };
