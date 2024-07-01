37b921e232913357bf5ca3606b1e531d9f54d0cb
AttilaTech01 <d.hains@attila-technologies.com>
2024-07-01 11:34:14 -0400

    Loading fix for Button and User page

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -21,36 +21,31 @@ function UsersList() {
         doAddUser();
     };
 
+    let content;
     if (isLoadingUsers) {
-        return <Skeleton times={6} className="h-10 w-full" />;
-    }
-
-    if (loadingUsersError) {
-        return <div>Error while fetching data ...</div>;
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center curser-pointer">
-                    {user.name}
+        content = <Skeleton times={6} className="h-10 w-full" />;
+    } else if (loadingUsersError) {
+        content = <div>Error while fetching data ...</div>;
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center curser-pointer">
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        )
-    });
+            )
+        })
+    }
 
     return (
         <div>
             <div className="flex flex-row justify-between m-3">
-                <h1 className="m-2 text-xl">
-                { isAddingUser 
-                    ? 'Adding user...'
-                    : <Button onClick={handleAddUser}>+ Add User</Button> 
-                }
+                <h1 className="m-2 text-xl">Users</h1>
+                <Button loading={isAddingUser} onClick={handleAddUser}>+ Add User</Button>
                 { addingUserError && 'Error adding user...' }    
-                </h1>
             </div>
-            {renderedUsers}
+            {content}
         </div>
     );
 };
